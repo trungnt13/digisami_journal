e0a915299a1caea9e7b5abe9d9b9431a32a8b51f
TrungNT <nickartin13@gmail.com>
2018-05-21 00:29:20 +0300

    Update new contents and .bib

--- src/acoustic_visualization.py
+++ src/acoustic_visualization.py
@@ -27,13 +27,12 @@ import matplotlib.mlab as mlab
 from matplotlib import pyplot as plt
 
 import os
-import random
 from six.moves import cPickle
 from collections import defaultdict, OrderedDict
 
 import numpy as np
 
-from odin.utils import Progbar
+from odin.utils import Progbar, UnitTimer
 from odin import visual as V
 from odin import fuel as F
 from odin.stats import freqcount
@@ -53,7 +52,6 @@ popular_ltype = ['fl, b', 'fl, e', 'fl, m',
                  'st, b', 'st, e', 'st, m']
 SEED = 5218
 np.random.seed(SEED)
-random.seed(SEED)
 
 
 def timeFMT(dur):
@@ -89,7 +87,7 @@ def get_nonzeros(x):
 # ===========================================================================
 # laugh, gender, utt_duration
 ds = F.Dataset(featpath, read_only=True)
-all_indices = list(ds['indices'].items())
+all_indices = sorted(ds['indices'].items(), key=lambda x: x[0])
 np.random.shuffle(all_indices)
 print(ds)
 # ====== get length ====== #
@@ -99,7 +97,25 @@ print(ds)
 #                                mode='all', ncpu=None,
 #                                return_single_data=True)
 # data.set_batch(batch_size=512, batch_mode='file', seed=None)
-
+def iterate_laughter(feat, target_language=None):
+  """ lang, laugh, spk, gender, feat """
+  for name, (start, end) in all_indices:
+    lang = name.split('/')[0]
+    # check target language
+    if target_language is not None:
+      if lang != str(target_language):
+        continue
+    # iterate
+    for s, e, laugh, spk in ds['laugh'][name]:
+      if laugh not in popular_ltype: # only for popular
+        continue
+      # get gender
+      gender = ds['gender'][lang][spk]
+      if isinstance(feat, (tuple, list)):
+        x = [ds[name][start:end][s:e] for name in feat]
+      else:
+        x = ds[feat][start:end][s:e]
+      yield lang, laugh, spk, gender, x
 # ===========================================================================
 # Calculate laugh pitch and f0, energy
 # ===========================================================================
@@ -120,42 +136,34 @@ min_pitch = np.finfo(np.float32).max
 min_energy = np.finfo(np.float32).max
 min_loud = np.finfo(np.float32).max
 # ====== get all data ====== #
-for name, (start, end) in all_indices:
-  lang = name.split('/')[0]
-  # check target language
-  if target_language is not None:
-    if lang != str(target_language):
-      continue
-  for s, e, laugh, spk in ds['laugh'][name]:
-    if laugh not in popular_ltype: # only for popular
-      continue
-    gender = ds['gender'][lang][spk]
-    # pitch
-    pitch = get_nonzeros(ds['pitch'][start:end][s:e][:, 0])
-    if len(pitch) > 0:
-      max_pitch = max(max_pitch, max(pitch))
-      min_pitch = min(min_pitch, min(pitch))
-    # f0
-    f0 = get_nonzeros(ds['f0'][start:end][s:e][:, 0])
-    if len(f0) > 0:
-      max_f0 = max(max_f0, max(f0))
-      min_f0 = min(min_f0, min(f0))
-    # loud
-    loud = get_nonzeros(ds['loudness'][start:end][s:e][:, 0])
-    loud = loud[loud > 0.12]
-    if len(loud) > 0:
-      max_loud = max(max_loud, max(loud))
-      min_loud = min(min_loud, min(loud))
-    # energy
-    energy = loudness2intensity(loud)
-    if len(energy) > 0:
-      max_energy = max(max_energy, max(energy))
-      min_energy = min(min_energy, min(energy))
-    # save info
-    laugh_pitch[gender][laugh] += pitch.tolist()
-    laugh_f0[gender][laugh] += f0.tolist()
-    laugh_energy[gender][laugh] += energy.tolist()
-    laugh_loud[gender][laugh] += loud.tolist()
+for lang, laugh, spk, gender, (pitch, f0, loud) in iterate_laughter(
+    feat=('pitch', 'f0', 'loudness'), target_language=None):
+  # pitch
+  pitch = get_nonzeros(pitch[:, 0])
+  if len(pitch) > 0:
+    max_pitch = max(max_pitch, max(pitch))
+    min_pitch = min(min_pitch, min(pitch))
+  # f0
+  f0 = get_nonzeros(f0[:, 0])
+  if len(f0) > 0:
+    max_f0 = max(max_f0, max(f0))
+    min_f0 = min(min_f0, min(f0))
+  # loud
+  loud = get_nonzeros(loud[:, 0])
+  loud = loud[loud > 0.12]
+  if len(loud) > 0:
+    max_loud = max(max_loud, max(loud))
+    min_loud = min(min_loud, min(loud))
+  # energy
+  energy = loudness2intensity(loud)
+  if len(energy) > 0:
+    max_energy = max(max_energy, max(energy))
+    min_energy = min(min_energy, min(energy))
+  # save info
+  laugh_pitch[gender][laugh] += pitch.tolist()
+  laugh_f0[gender][laugh] += f0.tolist()
+  laugh_energy[gender][laugh] += energy.tolist()
+  laugh_loud[gender][laugh] += loud.tolist()
 # ====== ploting helper====== #
 def plotting(data, title, ymin, ymax, step=80, outlier=(0, 0)):
   """ outlier: tuple (min, max) """
@@ -224,50 +232,51 @@ def norm_0_1(x):
 laugh_spec = defaultdict(lambda: defaultdict(list))
 max_spec = np.finfo(np.float32).min
 min_spec = np.finfo(np.float32).max
-samples1 = {}
-samples2 = {}
+samples1 = defaultdict(list)
+samples2 = defaultdict(list)
 # ====== get all data ====== #
-for name, (start, end) in all_indices:
-  lang = name.split('/')[0]
-  for s, e, laugh, spk in ds['laugh'][name]:
-    if laugh not in popular_ltype: # only for popular
-      continue
-    # get gender
-    gender = ds['gender'][lang][spk]
-    # get spectrogram
-    spec = ds['mspec'][start:end]
-    spec1 = spec[:, :40][s:e]
-    spec2 = spec[:, 40:80][s:e]
-    # save some samples
-    sample_name = gender + '/' + laugh
-    if sample_name not in samples1 and \
-    np.random.rand() > 0.08 and \
-    80 < spec1.shape[0] < 120:
-      samples1[sample_name] = spec1
-      samples2[sample_name] = spec2
-    # laugh_spec
-    if spec1.shape[0] > 0:
-      spec = norm_0_1(spec[:, :40])
-      spec = spec[s:e]
-      laugh_spec[gender][laugh].append(spec)
-    # store min, max
-    max_spec = max(max_loud, np.max(spec))
-    min_spec = min(min_loud, np.min(spec))
+for lang, laugh, spk, gender, spec in iterate_laughter('mspec'):
+  # get spectrogram
+  spec1 = spec[:, :40]
+  spec2 = spec[:, 40:80]
+  # save some samples
+  sample_name = gender + '/' + laugh
+  if len(samples1[sample_name]) < 2 and \
+  np.random.rand() > 0.08 and \
+  80 < spec1.shape[0] < 130:
+    samples1[sample_name].append(spec1)
+    samples2[sample_name].append(spec2)
+  # laugh_spec
+  if spec1.shape[0] > 0:
+    spec = norm_0_1(spec[:, :40])
+    laugh_spec[gender][laugh].append(spec)
+  # store min, max
+  max_spec = max(max_loud, np.max(spec))
+  min_spec = min(min_loud, np.min(spec))
 # ====== simple check ====== #
-assert len(samples1) == len(popular_ltype) * 2
-assert len(samples2) == len(popular_ltype) * 2
-# ====== plot sample first ====== #
+for n, s in sorted(samples1.items(), key=lambda x: x[0]):
+  assert len(s) == 2
+# ====== plot samples first ====== #
 for samples, figname in zip([samples1, samples2],
                             ['Mel-scaled spectrogram', 'Delta mel-spectrogram']):
-  plt.figure(figsize=(6, 2))
-  samples = sorted(samples.items(), key=lambda x: x[0])
-  for i, (name, spec) in enumerate(samples):
+  plt.figure(figsize=(6, 4))
+  for idx, (name, (spec1, spec2)) in enumerate(
+      sorted(samples.items(), key=lambda x: x[0])):
     gender, laugh = name.split('/')
-    plt.subplot(2, len(popular_ltype), i + 1)
-    V.plot_spectrogram(spec.T)
-    if i in (0, 6):
+    row = 1 if gender == 'F' else len(popular_ltype) * 2 + 1
+    idx = row + idx % len(popular_ltype)
+    # first row
+    plt.subplot(4, len(popular_ltype), idx)
+    V.plot_spectrogram(spec1.T)
+    if idx in (1, len(popular_ltype) * 2 + 1):
       plt.ylabel('Female' if gender == 'F' else 'Male', fontsize=10)
-    plt.title(laugh, fontsize=10)
+    # only title for first row
+    if gender == 'F':
+      plt.title(laugh, fontsize=10)
+    # second row
+    plt.subplot(4, len(popular_ltype), idx + len(popular_ltype))
+    V.plot_spectrogram(spec2.T)
+  # main title
   plt.suptitle(figname, fontsize=12)
   plt.subplots_adjust(wspace=0.04, hspace=0.06)
 # ====== spectrogram at different frequencies ====== #
@@ -310,9 +319,130 @@ plt.subplots_adjust(wspace=0.09, hspace=0.06)
 # PCA
 # ===========================================================================
 from sklearn.manifold import TSNE
-FEAT = 'mspec'
-n_components = None
-# ====== fast check the dataset ====== #
+# ====== color and marker ====== #
+styles = {gender + '/' + laugh: (color, marker)
+  for laugh, color in zip(popular_ltype,
+                          V.generate_random_colors(n=len(popular_ltype),
+                                                   seed=5218))
+  for gender, marker in zip(['F', 'M'], ['o', '^'])}
+legend = {j: i.replace('/', ':') for i, j in styles.items()}
+
+styles_flst = {gender + '/' + laugh: ('red' if 'fl,' in laugh else 'blue', marker)
+  for laugh in popular_ltype
+  for gender, marker in zip(['F', 'M'], ['o', '^'])}
+legend_flst = {s: name[:4] for name, s in styles_flst.items()}
+# ====== helper ====== #
+def cluster_visualization(feat_name, n_components, perp=30.0,
+                          pca_plot=True):
+  pca = ds['pca_' + feat_name]
+  tsne = TSNE(n_components=2, perplexity=perp,
+              early_exaggeration=12.0, learning_rate=120.0,
+              n_iter=1200,
+              n_iter_without_progress=300,
+              min_grad_norm=1e-7,
+              metric="euclidean", init="random", verbose=0,
+              random_state=5128, method='barnes_hut', angle=0.5)
+  # transform data
+  laugh_feat = defaultdict(list)
+  for lang, laugh, spk, gender, x in iterate_laughter(feat_name):
+    x = np.mean(pca.transform(x, n_components=n_components),
+                axis=0, keepdims=True)
+    name = gender + '/' + laugh
+    laugh_feat[name].append(x)
+  # merge all samples
+  laugh_pca = {i: np.concatenate(j, axis=0)
+               for i, j in laugh_feat.items()}
+  # post processing
+  X = []
+  color = []; marker = []
+  color_flst = []; marker_flst = []
+  for i, j in laugh_pca.items():
+    X.append(j)
+    n = len(j)
+    color += [styles[i][0]] * n
+    marker += [styles[i][1]] * n
+    color_flst += [styles_flst[i][0]] * n
+    marker_flst += [styles_flst[i][1]] * n
+  X = np.concatenate(X, axis=0)
+  # plot PCA
+  if pca_plot:
+    plt.figure()
+    V.plot_scatter(X[:, 0], X[:, 1], size=4.0,
+                   color=color, marker=marker,
+                   legend=legend,
+                   legend_ncol=len(popular_ltype),
+                   fontsize=10)
+    plt.xticks([])
+    plt.yticks([])
+    plt.suptitle('2D "%s" PCA visualization of laughter and gender' % feat_name)
+  # TSNE fitting
+  with UnitTimer(name='TSNE fit_transform: %s' % feat_name):
+    X = tsne.fit_transform(X)
+  # plot TNSE all laugh
+  plt.figure()
+  V.plot_scatter(X[:, 0], X[:, 1], size=4.0,
+                 color=color, marker=marker,
+                 legend=legend,
+                 legend_ncol=len(popular_ltype),
+                 fontsize=10)
+  if n_components is None:
+    n_components = ds[feat_name].shape[1]
+  plt.title('PCA-#components:%d  Perplexity:%d' % (n_components, perp))
+  plt.xticks([])
+  plt.yticks([])
+  plt.suptitle(
+      '2D "%s" T-SNE visualization of laughter & gender' % feat_name)
+  # plot TNSE flst
+  plt.figure()
+  V.plot_scatter(X[:, 0], X[:, 1], size=4.0,
+                 color=color_flst, marker=marker_flst,
+                 legend=legend_flst,
+                 legend_ncol=len(popular_ltype),
+                 fontsize=10)
+  if n_components is None:
+    n_components = ds[feat_name].shape[1]
+  plt.title('PCA-#components:%d  Perplexity:%d' % (n_components, perp))
+  plt.xticks([])
+  plt.yticks([])
+  plt.suptitle(
+      '2D "%s" T-SNE visualization of laughter & gender' % feat_name)
+# ====== test ====== #
+cluster_visualization('mspec', n_components=10, perp=10, pca_plot=True)
+cluster_visualization('mspec', n_components=10, perp=20, pca_plot=False)
+cluster_visualization('mspec', n_components=10, perp=30, pca_plot=False)
+cluster_visualization('mspec', n_components=20, perp=10, pca_plot=False)
+cluster_visualization('mspec', n_components=20, perp=20, pca_plot=False)
+cluster_visualization('mspec', n_components=20, perp=30, pca_plot=False)
+
+cluster_visualization('bnf', n_components=10, perp=10, pca_plot=True)
+cluster_visualization('bnf', n_components=10, perp=20, pca_plot=False)
+cluster_visualization('bnf', n_components=10, perp=30, pca_plot=False)
+cluster_visualization('bnf', n_components=20, perp=10, pca_plot=False)
+cluster_visualization('bnf', n_components=20, perp=20, pca_plot=False)
+cluster_visualization('bnf', n_components=20, perp=30, pca_plot=False)
+
+cluster_visualization('mfcc', n_components=10, perp=10, pca_plot=True)
+cluster_visualization('mfcc', n_components=10, perp=20, pca_plot=False)
+cluster_visualization('mfcc', n_components=10, perp=30, pca_plot=False)
+cluster_visualization('mfcc', n_components=20, perp=10, pca_plot=False)
+cluster_visualization('mfcc', n_components=20, perp=20, pca_plot=False)
+cluster_visualization('mfcc', n_components=20, perp=30, pca_plot=False)
+
+cluster_visualization('f0', n_components=None, perp=5, pca_plot=True)
+cluster_visualization('f0', n_components=None, perp=10, pca_plot=False)
+cluster_visualization('f0', n_components=None, perp=20, pca_plot=False)
+cluster_visualization('f0', n_components=None, perp=30, pca_plot=False)
+cluster_visualization('f0', n_components=None, perp=50, pca_plot=False)
+
+cluster_visualization('loudness', n_components=None, perp=5, pca_plot=True)
+cluster_visualization('loudness', n_components=None, perp=10, pca_plot=False)
+cluster_visualization('loudness', n_components=None, perp=20, pca_plot=False)
+cluster_visualization('loudness', n_components=None, perp=30, pca_plot=False)
+cluster_visualization('loudness', n_components=None, perp=50, pca_plot=False)
+
+# ===========================================================================
+# Others
+# ===========================================================================
 # f = all_files[12]
 # start, end = ds['indices'][f]
 # n = end - start
